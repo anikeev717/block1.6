275e8a2b949152602dc20bf438b97154e327acf9
Yuri Anikeev <anikeev717@gmail.com>
2023-07-19 23:06:55 +0300

    mentor fix 2

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,45 +1,96 @@
 import Swiper from 'swiper/swiper-bundle'
 
-const swiperService = new Swiper('.service__swiper', {
-  breakpoints: {
-    320: {
-      grid: {
-        fill: 'row',
-        rows: 1
-      },
-      slidesPerView: 1.3,
-      slidesPerGroup: 1,
-      spaceBetween: 16
-    },
-    480: {
-      grid: {
-        fill: 'row',
-        rows: 1
-      },
-      slidesPerView: 2,
-      slidesPerGroup: 1,
-      spaceBetween: 8
-    },
-    768: {
-      grid: {
-        fill: 'row',
-        rows: 1
-      },
-      slidesPerView: 3,
-      slidesPerGroup: 1,
-      spaceBetween: 4
-    },
-    1366: {
-      grid: {
-        fill: 'row',
-        rows: 2
-      },
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-      spaceBetween: 0
-    }
+// const swiperService = new Swiper('.service__swiper', {
+//   breakpoints: {
+//     320: {
+//       grid: {
+//         fill: 'row',
+//         rows: 1
+//       },
+//       slidesPerView: 'auto',
+//       slidesPerGroup: 1,
+//       spaceBetween: 16
+//     },
+//     480: {
+//       grid: {
+//         fill: 'row',
+//         rows: 1
+//       },
+//       slidesPerView: 'auto',
+//       slidesPerGroup: 1,
+//       spaceBetween: 8
+//     },
+//     768: {
+//       grid: {
+//         fill: 'row',
+//         rows: 1
+//       },
+//       slidesPerView: 'auto',
+//       slidesPerGroup: 1,
+//       spaceBetween: 4
+//     },
+//     1366: {
+//       grid: {
+//         fill: 'row',
+//         rows: 2
+//       },
+//       slidesPerView: 'auto',
+//       slidesPerGroup: 5,
+//       spaceBetween: 0
+//     }
+//   }
+// })
+
+let swiperService = null
+const swiperServiceCreate = () => {
+  if (!swiperService) {
+    swiperService = new Swiper('.service__swiper', {
+      breakpoints: {
+        320: {
+          grid: {
+            fill: 'row',
+            rows: 1
+          },
+          slidesPerView: 'auto',
+          slidesPerGroup: 1,
+          spaceBetween: 16
+        },
+        480: {
+          grid: {
+            fill: 'row',
+            rows: 1
+          },
+          slidesPerView: 'auto',
+          slidesPerGroup: 1,
+          spaceBetween: 8
+        },
+        768: {
+          grid: {
+            fill: 'row',
+            rows: 1
+          },
+          slidesPerView: 'auto',
+          slidesPerGroup: 1,
+          spaceBetween: 4
+        }
+      }
+    })
   }
-})
+}
+const swiperServiceDestroy = () => {
+  if (swiperService) {
+    swiperService.destroy()
+    swiperService = null
+  }
+}
+const resizeHandlerSlider = () => {
+  if (document.body.clientWidth < 1366) swiperServiceCreate()
+  else {
+    swiperServiceDestroy()
+  }
+}
+resizeHandlerSlider()
+window.addEventListener('resize', resizeHandlerSlider)
 
 const swiperBrands = new Swiper('.brands__swiper', {
   pagination: {
